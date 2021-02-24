846c5ce583c99f38ced9a296145d96130404a597
kaiishityou006 <619278692@qq.com>
2021-02-24 21:09:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fxokeb-w6hdwh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fxokeb-w6hdwh)
---
+++ src/app/return-product-list/return-product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-return-product-list',
+  templateUrl: './return-product-list.component.html',
+  styleUrls: ['./return-product-list.component.css']
+})
+export class ReturnProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
